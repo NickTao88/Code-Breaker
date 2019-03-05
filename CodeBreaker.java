8416431bae9dcc76e9df44a44ba319379d3972bf
NickTao88 <34190822+NickTao88@users.noreply.github.com>
2019-03-04 20:24:26 -0500

    Create CodeBreaker.java

---
+++ CodeBreaker.java
@@ -0,0 +1,23 @@
+/*
+ * Name: Nicholas Tao and Rohan Ravindran
+ * Date: March 4, 2019
+ * Assignment Name: Code Breaker
+ */
+
+public class CodeBreaker {
+  public static void main (String [] args) {
+    int num  = 0;
+   test (num);
+  }
+  public static int test (int num) {
+     /**
+     * returns num plus one.
+     * @param item element to search for
+     * @return the index of the first occurrence of the
+     * specified element in this list, or -1 if
+     * this list does not contain the element 
+     */ 
+    num++;
+    return num;
+  }
+}
