5d2d55f2eb9a91faf551fe5e889c36d1fc0b81dd
NickTao88 <34190822+NickTao88@users.noreply.github.com>
2019-03-06 09:58:47 -0500

    Update CodeBreaker.java

--- CodeBreaker.java
+++ CodeBreaker.java
@@ -19,9 +19,8 @@ public class CodeBreaker {
     
     createCode(VALID_CHARS, SIZE, code);
     for (int i = 0; i < TRIES; i++) {
-      String [] guess = userInput(SIZE);
-      //valid(); (use do-while to keep calling if invalid input)
-      ArrayList <String> countFullyCorr = findFullyCorrect (code, guess);
+      String [][] guess = userInput(SIZE, TRIES, i, VALID_CHARS); 
+      ArrayList <String> countFullyCorr = findFullyCorrect (code, guess[i]);
       if (countFullyCorr.equals(done)){
         userWins =true;
         System.out.println("Congratulations! It took you " +(i+1) +" guess to find the code");
@@ -29,7 +28,7 @@ public class CodeBreaker {
       }
       
       System.out.println(countFullyCorr);
-      removeFullyCorrect (code, guess);
+      removeFullyCorrect (code, guess[i]);
       //findColourCorrect (code, guess);
       //displayGame();
     }
@@ -39,18 +38,44 @@ public class CodeBreaker {
         System.out.print(code[i]);
       }
     }
-    
   }
-  
-  public static String[] userInput (int size) {
+  public static String[][] userInput (int size, int TRIES, int count, String colour) {
     Scanner sc = new Scanner (System.in);
-    String [] guess = new String [size];
-    for (int i = 0; i < size; i++) {
-      System.out.println("Enter your guess [G,R,B,Y,O,P]");
-      guess[i] = sc.nextLine();
+    String guess [][] = new String [TRIES][size];
+    boolean isValid;
+    
+    for (int j = 0; j < guess[count].length; j++) {
+      System.out.println("Please enter your guess of length " +size +" using the letters [G,R,B,Y,O,P]");
+      //String temp = sc.nextLine
+      guess[count][j] = sc.nextLine();
     }
+    isValid = valid (guess[count], colour, size );
+    
+    if (isValid==false) {
+      System.err.println("Invalid Input! Try Again!");
+      userInput (size, TRIES, count, colour);
+    }
+    
     //valid(guess);
     return guess;
+  }
+  
+  public static boolean valid(String[] userInput, String colour, int size) {
+    boolean lengthValid = false;
+    if (userInput.length == size) {
+      lengthValid = true;
+    }
+    boolean colourValid = true;
+    for (int i=0; i<userInput.length; i++) {
+      if (!colour.contains(userInput[i])) {
+        colourValid = false;
+      }
+    }
+    if (lengthValid == true && colourValid == true) {
+      return true;
+    } else {
+      return false;
+    }
   }
   
   public static String[] createCode (String colours, int size, String[] code) {
@@ -86,4 +111,35 @@ public class CodeBreaker {
     }
     return fullyCorrect;
   }
+  
+  
+  
+  public static boolean valid(ArrayList<String> userInput, String colour, int size) {
+    boolean lengthValid = false;
+    if (userInput.size() == size) {
+      lengthValid = true;
+    }
+    boolean colourValid = true;
+    for (int i=0; i<userInput.size(); i++) {
+      if (!colour.contains(userInput.get(i))) {
+        colourValid = false;
+      }
+    }
+    if (lengthValid == true && colourValid == true) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  public static ArrayList<String> duplicates(ArrayList<String> userInput) {
+    for (int i=0; i<userInput.size(); i++) {
+      for (int j=0; j<userInput.size(); j++) {
+        if (userInput.get(i).equals(userInput.get(j))) {
+          userInput.remove(i);
+        }
+      }
+    }
+    return userInput; 
+  } 
 }
