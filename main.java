674de510337355e61f6aa0ed7fe4dce6bcd73037
roro2002 <rohan.ravindran24@gmail.com>
2019-03-06 22:34:12 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,22 +1,75 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class main {
+	
+	static final int SIZE = 4;
+	  static final int TRIES = 10;
+	  static String clues [][] = new String [TRIES][SIZE];
+	
 	public static void main(String args[]) {
 		
 	}
 	
-	public static String[] findColourCorrect(String[] code, String[] userInput) { //change input for only 
-		String[] codeCopy = code.clone();
+	public static String[][] findColourCorrect(String[] code, String[] userInput, int global) { //change input for only 
+		String[] codeCopy = code.clone(); //test without clone
 		String[] colourCorrect = new String[code.length]; //har
+		int countClues = 0;
 		for (int i=0; i<userInput.length; i++) {
 			for (int j=0; j<code.length; j++) {
 				if (userInput[i].equals(codeCopy[j])) {
-					colourCorrect[i] = "w";
+					clues[global][countClues] = "w";
 					codeCopy[j] = null;
+					countClues++;
 					break;
 				}
 			}
 		}
-		return colourCorrect;
+		return clues;
 	}
+	
+	public static String[][] findFullyCorrect (String [] code, String [] guess, int i) {
+	    int countClues=0;
+
+	    for (int j = 0; j < code.length; j++) {
+	      if (code[j].equals(guess[j])) {
+	        countClues++;
+	        clues[i][countClues]="b";
+	      }
+	    }
+	    return clues;
+	  }
+	
+	
+	public static String[] createCode (String colours, int size, String[] code) {
+	    Random ran = new Random ();
+	    
+	    for (int i = 0; i < size; i++) {
+	      int num = ran.nextInt(size-1);
+	      char c = colours.charAt(num);
+	      String s = String.valueOf(c);
+	      code [i] = s;
+	    }
+	    return code; 
+	  }
+	
+	public static String[][] userInput (int size, int TRIES, int count, String colour) {
+	    Scanner sc = new Scanner (System.in);
+	    String guess [][] = new String [TRIES][size];
+	    boolean isValid;
+	    
+	    for (int j = 0; j < guess[count].length; j++) {
+	      System.out.println("Please enter your guess of length " +size +" using the letters [G,R,B,Y,O,P]");
+	      guess[count][j] = sc.nextLine();
+	    }
+	    isValid = valid (guess[count], colour, size );
+	    
+	    if (isValid==false) {
+	      System.err.println("Invalid Input! Try Again!");
+	      userInput (size, TRIES, count, colour);
+	    }
+	    return guess;
+	  }
 	
 	public static boolean valid(String[] userInput, String colour, int size) {
 		boolean colourValid = true;
