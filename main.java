396deeba64088f13d6a6b012f404d5d81e108726
roro2002 <rohan.ravindran24@gmail.com>
2019-03-06 21:49:27 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,65 +1,58 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-
 public class main {
 	public static void main(String args[]) {
 		
-		String[] code = {"G", "O", "R"};
-		ArrayList<String> userInput = new ArrayList<String>();
-		userInput.add("Y");
-		userInput.add("G");
-		userInput.add("G"); 
-		final String colour = "GRBYOP";
-		
-		System.out.println(findColourCorrect(code, userInput));
-		//System.out.println(valid(userInput, colour, 4));
-		
 	}
 	
-	public static ArrayList<String> findColourCorrect(String[] code, ArrayList<String> userInput) { //change input for only 
-		userInput = duplicates(userInput);
-		ArrayList<String> colourCorrect = new ArrayList<String>();
-		boolean tempTrue = false;
-		for (int i=0; i<userInput.size(); i++) {
+	public static String[] findColourCorrect(String[] code, String[] userInput) { //change input for only 
+		String[] codeCopy = code.clone();
+		String[] colourCorrect = new String[code.length]; //har
+		for (int i=0; i<userInput.length; i++) {
 			for (int j=0; j<code.length; j++) {
-				if (code[j].equals(userInput.get(i))) {
-					tempTrue = true;
+				if (userInput[i].equals(codeCopy[j])) {
+					colourCorrect[i] = "w";
+					codeCopy[j] = null;
+					break;
 				}
 			}
-			if (tempTrue) {
-				colourCorrect.add("w");
-			}
-			tempTrue = false;
 		}
 		return colourCorrect;
 	}
 	
-	public static boolean valid(ArrayList<String> userInput, String colour, int size) {
-		boolean lengthValid = false;
-		if (userInput.size() == size) {
-			lengthValid = true;
-		}
+	public static boolean valid(String[] userInput, String colour, int size) {
 		boolean colourValid = true;
-		for (int i=0; i<userInput.size(); i++) {
-			if (!colour.contains(userInput.get(i))) {
-					colourValid = false;
+		for (int i=0; i<userInput.length; i++) {
+			if (colour.indexOf(userInput[i]) < 0) {
+				colourValid = false;
+				break;
 			}
 		}
-		if (lengthValid == true && colourValid == true) {
+		if (userInput.length == size && colourValid == true) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	public static ArrayList<String> duplicates(ArrayList<String> userInput) {
-		for (int i=0; i<userInput.size(); i++) {
-			for (int j=0; j<userInput.size(); j++) {
-				if (userInput.get(i).equals(userInput.get(j))) {
-					userInput.remove(i);
-				}
-			}
+	public static void displayGame(String[][] board, String[][] piecesCorrect) {
+		System.out.println("Guess	   Clues");
+		for (int i=0; i<16; i++) {
+			System.out.print("*");
 		}
-		return userInput;	
-	}	
+		System.out.println();
+		
+		for (int i=0; i<board.length; i++) {
+			for (int j=0; j<board[i].length; j++) {
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.print("	   ");
+			for (int k=0; k<piecesCorrect[i].length; k++) {
+				if (piecesCorrect[i][k] != null) {
+					System.out.print(piecesCorrect[i][k] + " ");
+				}
+				
+			}
+			System.out.println();
+		}
+	}
+		
 }
