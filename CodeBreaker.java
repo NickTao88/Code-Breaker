592ab82d7184796671c15cafe665d86f0a25f0db
NickTao88 <34190822+NickTao88@users.noreply.github.com>
2019-03-12 21:40:34 -0400

    Update CodeBreaker.java

--- CodeBreaker.java
+++ CodeBreaker.java
@@ -16,17 +16,17 @@ public class CodeBreaker5 {
   static int countClues=0;
   static Scanner sc = new Scanner (System.in);
   static String [] remCode;
+  static int countCurrTurn = 0;
   
   public static void main(String [] args) {
     final String VALID_CHARS = "GRBYOP";
-    String [ ] code = new String [SIZE];
     boolean userWins = false;
     
     System.out.println("Welcome to Code Breaker! Enter your name");
     //String name = sc.nextLine();
     //System.out.println("Hey " +name +"! Let's get started!");
     
-    createCode(VALID_CHARS, SIZE, code);
+    String [] code = createCode(VALID_CHARS, SIZE);
     
     for (int currTurn = 0; currTurn < TRIES; currTurn++) {
       
@@ -50,7 +50,7 @@ public class CodeBreaker5 {
         validGuesses[currTurn][i] = guess[i];
       }
       
-      findFullyCorrect (code, guess, currTurn);
+      findFullyCorrect (code, guess);
       for (int a = 0; a < SIZE; a++) {
         if (clues[currTurn][a] == "b") {
           userWins = true;
@@ -66,17 +66,36 @@ public class CodeBreaker5 {
       String [] remFuCor = removeFullyCorrect (code, guess);
       
       
-      findColourCorrect(remCode, remFuCor , currTurn);
+      findColourCorrect(remCode, remFuCor);
       
-      displayGame(validGuesses, clues, currTurn);
+      displayGame(validGuesses, clues);
+      
+      countCurrTurn++;
     }
     if (userWins==false) {
-    System.out.print("I'm sorry, you lose. The correct code was ");
-    for (int i = 0; i < code.length;i++) {
-      System.out.print(code[i]);
+      System.out.print("I'm sorry, you lose. The correct code was ");
+      for (int i = 0; i < code.length;i++) {
+        System.out.print(code[i]);
+      }
+      System.out.println("");
     }
-    System.out.println("");
+  }
+  
+  public static String[] createCode (String colours, int size) {
+    Random ran = new Random ();
+    String [] code = new String [SIZE];
+    for (int i = 0; i < size; i++) {
+      int num = ran.nextInt(size-1);
+      char c = colours.charAt(num);
+      String s = String.valueOf(c);
+      code [i] = s;
     }
+    System.out.print("Code: "); //temporary
+    for (int i = 0; i < code.length; i++) {
+      System.out.print(code[i]); //temporary
+    }
+    System.out.println(""); //temporary
+    return code; 
   }
   
   public static boolean valid(String []userInput, String colour, int size) {
@@ -100,41 +119,25 @@ public class CodeBreaker5 {
     }
   }
   
-  public static String[] createCode (String colours, int size, String[] code) {
-    Random ran = new Random ();
-    
-    for (int i = 0; i < size; i++) {
-      int num = ran.nextInt(size-1);
-      char c = colours.charAt(num);
-      String s = String.valueOf(c);
-      code [i] = s;
-    }
-    System.out.print("Code: "); //temporary
-    for (int i = 0; i < code.length; i++) {
-      System.out.print(code[i]); //temporary
-    }
-    System.out.println(""); //temporary
-    return code; 
-  }
   
-  public static String[][] findFullyCorrect (String [] code, String [] guess, int i) {   
+  public static String[][] findFullyCorrect (String [] code, String [] guess) {   
     for (int j = 0; j < code.length; j++) {
       if (code[j].equals(guess[j])) {
-        clues[i][countClues]="b";
+        clues[countCurrTurn][countClues]="b";
         countClues++;
       }
     }
     return clues;
   }
   
-  public static String[] removeFullyCorrect (String [] code, String [] guess) { //fix
+  public static String[] removeFullyCorrect (String [] code, String [] guess) { 
     int temp = 0;
     String [] remFullyCorr = new String [code.length - countClues];
     remCode = new String [remFullyCorr.length];
     for (int i = 0; i < code.length; i++) {
       if (!code[i].equals(guess[i])) {
         remFullyCorr[temp] = guess[i];
-        remCode[temp] = code[i]; //INITIALIZED REMCODE HERE BUT STILL ENDS UP BEING NULL?
+        remCode[temp] = code[i]; 
         temp++;
       }
     }
@@ -143,12 +146,12 @@ public class CodeBreaker5 {
   }
   
   //THIS METHOD BROKEN
-  public static String[][] findColourCorrect(String[] code, String[] userInput, int count) {
+  public static String[][] findColourCorrect(String[] code, String[] userInput) {
     ArrayList<String> input = new ArrayList<String>(Arrays.asList(userInput));
     ArrayList<String> codeList = new ArrayList<String>(Arrays.asList(code));
     for (int i=0; i < codeList.size(); i++) {
       if (input.contains(codeList.get(i))) {
-        clues[count][countClues] = "w";
+        clues[countCurrTurn][countClues] = "w";
         countClues++;
         codeList.remove(i);
       }
@@ -157,12 +160,12 @@ public class CodeBreaker5 {
     return clues;
   }
   
-  public static void displayGame(String[][] board, String[][] piecesCorrect, int count) {
+  public static void displayGame(String[][] board, String[][] piecesCorrect) {
     System.out.println("Guess        Clues");
     
     System.out.println("****************");
     
-    for (int i=0; i<(count+1); i++) {
+    for (int i=0; i<(countCurrTurn+1); i++) {
       for (int j=0; j<board[i].length; j++) {
         System.out.print(board[i][j] + " ");
       }
