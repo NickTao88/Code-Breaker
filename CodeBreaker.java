a3e0da1ad9941c241be3d6dec68ffc43007fcd33
NickTao88 <34190822+NickTao88@users.noreply.github.com>
2019-03-12 21:59:25 -0400

    Update CodeBreaker.java

--- CodeBreaker.java
+++ CodeBreaker.java
@@ -2,13 +2,11 @@
  * Name: Nicholas Tao and Rohan Ravindran
  * Date: March 4, 2019
  * Assignment Name: Code Breaker
- * QUESTION FOR MR A: If invalid input, they re-enter ALL input? ie input:"BBBA" --> need to reenter all, not just the A?
- * TO DO:
- * need 2 arraylists (one is code after removing fullyCorr and one is guess after removing fullyCorr), then compare the two lists!
- * to do: take input as a one line string and store in a String array
  */
 import java.util.*;
 public class CodeBreaker5 {
+  
+  //declare global variables
   static final int SIZE = 4;
   static final int TRIES = 10;
   static String clues [][] = new String [TRIES][SIZE]; 
@@ -20,58 +18,75 @@ public class CodeBreaker5 {
   
   public static void main(String [] args) {
     final String VALID_CHARS = "GRBYOP";
-    boolean userWins = false;
+    boolean userWins = false; 
     
+    //welcome user and introduction
     System.out.println("Welcome to Code Breaker! Enter your name");
-    //String name = sc.nextLine();
-    //System.out.println("Hey " +name +"! Let's get started!");
+    String name = sc.nextLine();
+    System.out.println("Hey " +name +"! Let's get started!");
     
+    //generate code and store in array
     String [] code = createCode(VALID_CHARS, SIZE);
     
+    //main loop for game runs up to TRIES times
     for (int currTurn = 0; currTurn < TRIES; currTurn++) {
       
       boolean isValid;
+      
+      //prompt user for input
       System.out.println("Please enter your guess of length " +SIZE +" using the letters [G,R,B,Y,O,P]");
       String guessStr = sc.nextLine();
       
+      //declare array with length the length of the guess
       String[] guess = new String[guessStr.length()];
       
+      //store guess String in an array
       for (int i = 0; i < guessStr.length(); i++) {
         guess[i] = Character.toString(guessStr.charAt(i));
       }
       
-      isValid = valid (guess, VALID_CHARS, SIZE);
+      isValid = valid (guess, VALID_CHARS, SIZE); //check if input is valid
+      
+      //if input is invalid, run the loop again 
       if (isValid==false) {
-        System.err.println("Invalid Input! Try Again!");
+        System.err.println("Invalid Input! Try Again!"); //print error message
         currTurn--;
         continue;
       } 
+      
+      //store valid guesses in the 2D array (needed for display method)
       for (int i = 0; i < guess.length;i++) {
         validGuesses[currTurn][i] = guess[i];
       }
       
-      findFullyCorrect (code, guess);
+      findFullyCorrect (code, guess);//call method to find fully correct colours
+      
+      //check if the clues are all "b", meaning guess is fully correct
       for (int a = 0; a < SIZE; a++) {
         if (clues[currTurn][a] == "b") {
-          userWins = true;
+          userWins = true; 
         } else {
           userWins = false;
-          break;
+          break; //if one clue is not "b", stop checking (break)
         }
       }
+      
+      //if user wins, print conrgatulations method and number of guesses
       if (userWins==true){
         System.out.println("Congratulations! It took you " +(currTurn+1) +" guess to find the code");
-        break;
+        break; //exit loop
       }
-      String [] remFuCor = removeFullyCorrect (code, guess);
+     
+      String [] remFuCor = removeFullyCorrect (code, guess); //store the guess with the fully correct colours removed as an array
       
+      findColourCorrect(remCode, remFuCor); //find colour correct
       
-      findColourCorrect(remCode, remFuCor);
+      displayGame(validGuesses, clues); //display current game results
       
-      displayGame(validGuesses, clues);
-      
-      countCurrTurn++;
+      countCurrTurn++; //add one to counter which counts the current turn
     }
+    
+    //if user did not win, print losing message and correct code
     if (userWins==false) {
       System.out.print("I'm sorry, you lose. The correct code was ");
       for (int i = 0; i < code.length;i++) {
@@ -80,7 +95,15 @@ public class CodeBreaker5 {
       System.out.println("");
     }
   }
-  
+/**
+ * Returns an array of randomly generated characters (representing colours), 
+ * from a String of colours
+ *
+ * @param colours possible colours to generate a code from
+ * @param size length of code
+ * @return an array of randomly 
+ * generated characters (representing colours)
+ */
   public static String[] createCode (String colours, int size) {
     Random ran = new Random ();
     String [] code = new String [SIZE];
@@ -98,7 +121,18 @@ public class CodeBreaker5 {
     return code; 
   }
   
-  public static boolean valid(String []userInput, String colour, int size) {
+/**
+ * Returns true if guess is valid or
+ * false if guess is invalid
+ *
+ * @param userInput the guess from the user
+ * @param colour possible colours
+ * @param size length of code
+ * @return true if guess is valid (correct characters and size)
+ * or false if guess is invalid (incorrect characters and size)
+ */
+  
+  public static boolean valid(String [] userInput, String colour, int size) {
     boolean lengthValid = false;
     if (userInput.length == size) {
       lengthValid = true;
